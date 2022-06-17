b36b078a905bb8b504a46465d9dad9c2301740c8
Samara Caldas <samaraalbacaldas@hotmail.com>
2022-06-17 11:58:32 -0300

    Módulo 3

---
+++ Modulo III - JavaScript/teste.js
@@ -0,0 +1,15 @@
+console.log("hello word!");
+
+function returnEvenValues(array) {
+    let evenNums = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array [i] % 2 === 0) {
+            evenNums.push(array[i]);
+        }
+    }
+    console.log('os números pares sao:', evenNums);
+}
+
+let array = [1, 2, 4, 5, 7, 8];
+
+returnEvenValues(array);
