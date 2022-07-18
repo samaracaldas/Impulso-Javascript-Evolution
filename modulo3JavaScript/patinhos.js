89de265b6a0c46cdc865f4a579a118d56df71416
Samara Caldas <samaraalbacaldas@hotmail.com>
2022-07-18 10:57:51 -0300

    desafio type

---
+++ modulo3JavaScript/patinhos.js
@@ -0,0 +1,24 @@
+let N = parseInt(gets());
+let jogador1, jogador2;
+for (let i = 0; i < N; i++) {
+    jogador1 = gets();
+    jogador2 = gets();
+    		
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+    if(jogador1.toUpperCase() === "ATAQUE" && jogador2.toUpperCase() === "ATAQUE")
+      print("Aniquilacao mutua");
+    else if (jogador1.toUpperCase() === "PAPEL" && jogador2.toUpperCase() === "PAPEL")
+        print("Ambos venceram");
+    	
+    else if (jogador1.toUpperCase() === "ATAQUE" && jogador2.toUpperCase() === "PEDRA") print("Jogador 1 venceu");
+    else if (jelseogador1.toUpperCase() === "PEDRA" && jogador2.toUpperCase() === "ATAQUE") print("Jogador 2 venceu");
+    else if (jogador1.toUpperCase() === "PAPEL" && jogador2.toUpperCase() === "PEDRA") print("Jogador 2 venceu");
+    else if (jogador1.toUpperCase() === "ATAQUE" && jogador2.toUpperCase() === "PAPEL") print ("Jogador 1 venceu");
+    else if (jogador1.toUpperCase() === "PEDRA" && jogador2.toUpperCase() === "PAPEL") print ("Jogador 1 venceu");
+    else if (jogador1.toUpperCase() === "PAPEL" && jogador2.toUpperCase() === "ATAQUE") print ("Jogador 2 venceu");
+        
+    else print("Sem ganhador");
+}  
+
+console.log();
