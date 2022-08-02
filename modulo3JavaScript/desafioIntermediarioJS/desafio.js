f85a7b50375179f74395c2d4a197b5e615cdf228
Samara Caldas <samaraalbacaldas@hotmail.com>
2022-08-02 18:45:29 -0300

    desafio react

---
+++ modulo3JavaScript/desafioIntermediarioJS/desafio.js
@@ -0,0 +1,13 @@
+let n = parseInt.gets().split();
+let min = parseInt(gets().split());
+let max = parseInt(gets().split());
+
+
+   let altura, conta = 0;
+		for (let x=0 ; x<n ; x++) {
+            if( altura >= vet[1]  && altura <= vet[2])
+                conta = conta++;
+    
+            }
+  		print(conta);
+        
---
+++ modulo4React/testes/src/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import RectDOM from "react-dom"
+import "./styles.css"
+
+
+
+const App = ( ) => {
+
+    return {
+        <div className="App">
+            Hello World
+        </div>
+    }
+}
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App />, rootElement)
